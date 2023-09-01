ed6a21ac61eb21391e08b8dc41f2c88ce499f05e
Alexander Sweetman <sweetmanco@gmail.com>
2023-09-01 17:12:29 +0100

    more streamlit

--- app.py
+++ app.py
@@ -2,19 +2,23 @@ import requests
 import streamlit as st
 import numpy as np
 
-# url=''
+url=''
 
-user_input = st.file_uploader(
-    "Select a piece of artwork that you enjoy (png or jpg only)"
-    , type=['png','jpg','jpeg']
-    ,accept_multiple_files=False
-    )
+# user_input = st.file_uploader(
+#     "Select a piece of artwork that you enjoy (png or jpg only)"
+#     , type=['png','jpg','jpeg']
+#     ,accept_multiple_files=False
+#     )
 
-if user_input is not None:
-    query_h=st.text_input('height')
+
+query_h=st.text_input('height')
+query_w=st.text_input('width')
+
+# if user_input is not None:
+#     query_h=st.text_input('height')
     
-if user_input is not None:
-    query_w=st.text_input('width')
+# if user_input is not None:
+#     query_w=st.text_input('width')
 
 response=requests.get(f'http://127.0.0.1:8000/flip?height={query_h}&width={query_w}')
 st.write(response.json())   
--- main.py
+++ main.py
@@ -7,26 +7,27 @@ import tempfile
 
 app = FastAPI()
 
-@app.get("/")
-def table_flip():
-    return {'hello'}
+
+# @app.get("/")
+# def table_flip():
+#     return {'hello'}
 
 @app.get("/flip")
 def table_flip(height,width):
     return {int(height) * int(width)}
 
-@app.post("/imageflip")
-async def flip_image(image: UploadFile):
+# @app.post("/imageflip")
+# async def flip_image(image: UploadFile):
 
-    image_data = await image.read()
-    image = image.open(io,BytesIO(image_data)))
-    flipped_img = img.transpose(Image.FLIP_LEFT_RIGHT)
+#     image_data = await image.read()
+#     image = image.open(io,BytesIO(image_data)))
+#     flipped_img = img.transpose(Image.FLIP_LEFT_RIGHT)
 
-with tempFile.NamedTemporaryFile(suffix='.png', delete=False,) as temp_file:
-    flipped_img.save(temp_file, format='PNG')
-
-
+# with tempFile.NamedTemporaryFile(suffix='.png', delete=False,) as temp_file:
+#     flipped_img.save(temp_file, format='PNG')
 
 
-# async def root():
-#     return {"message": "Hello World"}
+
+
+async def root():
+    return {"message": "Hello World"}
